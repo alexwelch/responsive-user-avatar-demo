9ea2aafec4d790e61ae1b6f4490b22d8120263bb
Alex Welch <me@alexwelch.com>
2011-10-12 07:35:36 -0600

    Create some users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(:name => "Billy B")
+User.create(:name => "Jenny J")
+User.create(:name => "Kara K")
