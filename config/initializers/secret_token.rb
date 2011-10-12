7994c43d32d7052cd34599c56faf26b5b15ac6f9
Alex Welch <me@alexwelch.com>
2011-10-12 07:14:47 -0600

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResponsiveUserAvatar::Application.config.secret_token = '9d0e726d5ecfe3921c98f4ddcaaf6711bd0e470853c6078130f1bf94e1fb8d4513081c598a9f366a154a8a7f37a138bd3e0f7e0da9304706690e2dd55faa5d23'
