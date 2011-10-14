16333ea512a756dc1fd8d71b02872e439b4eb11c
Alex Welch <me@alexwelch.com>
2011-10-13 21:46:46 -0600

    Add monocle kitten.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,18 @@ user1 = User.find_or_create_by_name("Billy B")
 user2 = User.find_or_create_by_name("Jenny J")
 user3 = User.find_or_create_by_name("Kara K")
 
+Avatar.destroy_all
 Avatar.create(:user => user1,
               :small_url => "/assets/dfp/small.jpg",
               :medium_url => "/assets/dfp/medium.jpg",
               :large_url => "/assets/dfp/large.jpg")
 
 Avatar.create(:user => user2,
+              :small_url => "/assets/mono_cat/small.jpg",
+              :medium_url => "/assets/mono_cat/medium.jpg",
+              :large_url => "/assets/mono_cat/large.jpg")
+
+Avatar.create(:user => user3,
               :small_url => "/assets/rails.png",
               :medium_url => "/assets/rails.png",
               :large_url => "/assets/rails.png")
-
-Avatar.create(:user => user3,
-              :small_url => "/assets/dfp/small.jpg",
-              :medium_url => "/assets/dfp/medium.jpg",
-              :large_url => "/assets/dfp/large.jpg")
