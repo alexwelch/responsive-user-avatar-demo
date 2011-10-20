6cec2420b7b977e9cf090b57ca9586cde3f5df32
Alex Welch <me@alexwelch.com>
2011-10-19 18:05:09 -0600

    Specs test the send_file method, instead of the redirect method.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,48 +2,50 @@ require 'spec_helper'
 
 describe UsersController do
   describe "GET avatar" do
-    it "redirects to the small avatar for the user" do
-      user = User.create(:name => "Dog Lady")
-      user.create_avatar(
-        :small_url => "/assets/dfp/small.jpg",
-        :medium_url => "/assets/dfp/medium.jpg",
-        :large_url => "/assets/dfp/large.jpg")
-      request.stub(:env).and_return({"HTTP_REFERER" => "http://localhost:3000/user/#{user.id}"})
-      get :avatar, :size => :small
-      response.should redirect_to "/assets/dfp/small.jpg"
+    context "when the referer has an id" do
+      before do
+        @user = User.create(:name => "Dog Lady")
+        @user.create_avatar(
+          :small_path => "app/assets/images/dfp/small.jpg",
+          :medium_path => "app/assets/images/dfp/medium.jpg",
+          :large_path => "app/assets/images/dfp/large.jpg")
+        request.stub(:env).and_return({"HTTP_REFERER" => "http://localhost:3000/user/#{@user.id}"})
+        controller.stub(:render)
+      end
+
+      it "finds the user" do
+        get :avatar, :size => :small
+        assigns(:user).should == @user
+      end
+
+      it "renders the small avatar for the user" do
+        controller.should_receive(:send_file).with("#{Rails.root}/app/assets/images/dfp/small.jpg")
+        get :avatar, :size => :small
+      end
+
+      it "renders the medium avatar for the user" do
+        controller.should_receive(:send_file).with("#{Rails.root}/app/assets/images/dfp/medium.jpg")
+        get :avatar, :size => :medium
+      end
+
+      it "renders the large avatar for the user" do
+        controller.should_receive(:send_file).with("#{Rails.root}/app/assets/images/dfp/large.jpg")
+        get :avatar, :size => :large
+      end
+    end
+    context "when there is no referer" do
+      it "renders a 404" do
+        get :avatar, :size => :large
+        response.status.should == 404
+      end
     end
 
-    it "redirects to the medium avatar for the user" do
-      user = User.create(:name => "Dog Lady")
-      user.create_avatar(
-        :small_url => "/assets/dfp/small.jpg",
-        :medium_url => "/assets/dfp/medium.jpg",
-        :large_url => "/assets/dfp/large.jpg")
-      request.stub(:env).and_return({"HTTP_REFERER" => "http://localhost:3000/user/#{user.id}"})
-      get :avatar, :size => :medium
-      response.should redirect_to "/assets/dfp/medium.jpg"
-    end
-
-    it "redirects to the large avatar for the user" do
-      user = User.create(:name => "Dog Lady")
-      user.create_avatar(
-        :small_url => "/assets/dfp/small.jpg",
-        :medium_url => "/assets/dfp/large.jpg",
-        :large_url => "/assets/dfp/large.jpg")
-      request.stub(:env).and_return({"HTTP_REFERER" => "http://localhost:3000/user/#{user.id}"})
-      get :avatar, :size => :large
-      response.should redirect_to "/assets/dfp/large.jpg"
-    end
-
-    it "renders a 404 when there is no referer" do
-      get :avatar, :size => :large
-      response.status.should == 404
-    end
-
-    it "renders a 404 when there is an invalid referer" do
-      request.stub(:env).and_return({"HTTP_REFERER" => "http://localhost:3000/public/user/9999999999999999"})
-      get :avatar, :size => :large
-      response.status.should == 404
+    context "when there is an invalid referer" do
+      it "renders a 404" do
+        request.stub(:env).and_return({"HTTP_REFERER" => "http://localhost:3000/public/user/9999999999999999"})
+        get :avatar, :size => :large
+        response.status.should == 404
+      end
     end
   end
 end
