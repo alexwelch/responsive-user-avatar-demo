41a202739af07b020f5779d7afd7de0db6f88faf
Alex Welch <me@alexwelch.com>
2011-10-24 10:23:35 -0600

    the better way

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,23 +6,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def avatar
-    user = get_user
-    size = params[:size]
-    if user.present?
-      send_file "#{Rails.root}/#{user.avatar.url(size)}"
-    else
-      render :status => :not_found, :text => "not found"
-    end
-  end
-
-  private
-
-  def get_user
-    if request.env["HTTP_REFERER"].present?
-      user_id = request.env["HTTP_REFERER"].match(/\/(\d+)\/?/)[1]
-      user = User.find_by_id(user_id)
-    end
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,16 @@ user3 = User.find_or_create_by_name("Kara K")
 
 Avatar.destroy_all
 Avatar.create(:user => user1,
-              :small_url => "app/assets/images/dfp/small.jpg",
-              :medium_url => "app/assets/images/dfp/medium.jpg",
-              :large_url => "app/assets/images/dfp/large.jpg")
+              :small_url => "dfp/small.jpg",
+              :medium_url => "dfp/medium.jpg",
+              :large_url => "dfp/large.jpg")
 
 Avatar.create(:user => user2,
-              :small_url => "app/assets/images/mono_cat/small.jpg",
-              :medium_url => "app/assets/images/mono_cat/medium.jpg",
-              :large_url => "app/assets/images/mono_cat/large.jpg")
+              :small_url => "mono_cat/small.jpg",
+              :medium_url => "mono_cat/medium.jpg",
+              :large_url => "mono_cat/large.jpg")
 
 Avatar.create(:user => user3,
-              :small_url => "app/assets/images/rails.png",
-              :medium_url => "app/assets/images/rails.png",
-              :large_url => "app/assets/images/rails.png")
+              :small_url => "rails.png",
+              :medium_url => "rails.png",
+              :large_url => "rails.png")
